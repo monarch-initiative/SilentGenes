6166df16573cc67a65d8fc7bc892e1ac4041bb72
Daniel Danis <daniel.danis@jax.org>
2021-11-05 21:40:55 -0400

    Move `Coding.cdsLength` to private interface method. Update docs

--- gtram-model/src/main/java/xyz/ielis/gtram/model/Transcript.java
+++ gtram-model/src/main/java/xyz/ielis/gtram/model/Transcript.java
@@ -1,7 +1,8 @@
 package xyz.ielis.gtram.model;
 
 /**
- * The interface to represent Gencode tra
+ * The interface to represent (non)coding transcript. The transcript is located on reference genome, it consists
+ * of exons and introns, and can be {@link Coding} or non-coding.
  */
 public interface Transcript extends Located, Spliceable {
 
