3c97cd21d7ae984c4e109d4692376b923611bc96
Daniel Danis <daniel.danis@jax.org>
2021-11-09 16:29:01 -0500

    Implement `JannovarParser`

---
+++ silent-genes-jannovar/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module xyz.ielis.silent.genes.jannovar {
+    requires xyz.ielis.silent.genes.model;
+    requires jannovar.core;
+    requires com.google.common;
+    requires org.slf4j;
+
+    exports xyz.ielis.silent.genes.jannovar;
+}
---
+++ silent-genes-jannovar/src/main/java/xyz/ielis/silent/genes/jannovar/JannovarIterator.java
@@ -0,0 +1,149 @@
+package xyz.ielis.silent.genes.jannovar;
+
+import de.charite.compbio.jannovar.data.JannovarData;
+import de.charite.compbio.jannovar.reference.GenomeInterval;
+import de.charite.compbio.jannovar.reference.TranscriptModel;
+import org.monarchinitiative.svart.*;
+import xyz.ielis.silent.genes.model.Gene;
+import xyz.ielis.silent.genes.model.GeneIdentifier;
+import xyz.ielis.silent.genes.model.Transcript;
+import xyz.ielis.silent.genes.model.TranscriptIdentifier;
+
+import java.util.*;
+
+public class JannovarIterator implements Iterator<Gene> {
+
+    // Jannovar stores coordinates in 0-based system
+    private static final CoordinateSystem COORDINATE_SYSTEM = CoordinateSystem.zeroBased();
+    private final Iterator<Map.Entry<String, Collection<TranscriptModel>>> iterator;
+    private final GenomicAssembly assembly;
+
+    private Gene next;
+
+    JannovarIterator(JannovarData jannovarData, GenomicAssembly assembly) {
+        this.assembly = assembly;
+        Map<String, Collection<TranscriptModel>> geneMap = jannovarData.getTmByGeneSymbol().asMap();
+        iterator = geneMap.entrySet().iterator();
+        next = parseGene();
+    }
+
+    private static Strand parseStrand(de.charite.compbio.jannovar.reference.Strand strand) {
+        switch (strand) {
+            case FWD:
+                return Strand.POSITIVE;
+            case REV:
+                return Strand.NEGATIVE;
+            default:
+                throw new IllegalArgumentException(String.format("Unknown strand %s", strand));
+        }
+    }
+
+    private static GenomicRegion parseLocation(Contig contig, Strand strand, Collection<TranscriptModel> transcripts) {
+        int start = contig.length(), end = 0;
+
+        for (TranscriptModel tx : transcripts) {
+            GenomeInterval txRegion = tx.getTXRegion();
+            start = Math.min(start, txRegion.getBeginPos());
+            end = Math.max(end, txRegion.getEndPos());
+        }
+
+        return GenomicRegion.of(contig, strand, COORDINATE_SYSTEM, start, end);
+    }
+
+    private static Gene parseNextGene(GenomicAssembly assembly, Map.Entry<String, Collection<TranscriptModel>> entry) {
+        Collection<TranscriptModel> transcripts = entry.getValue();
+        if (transcripts.isEmpty())
+            return null; // no transcripts defined for the gene
+
+        TranscriptModel first = transcripts.iterator().next();
+        Map<String, String> altGeneIds = first.getAltGeneIDs();
+        String geneId = altGeneIds.get("ENSEMBL_GENE_ID");
+        if (geneId == null)
+            geneId = altGeneIds.get("HGNC_ID");
+        if (geneId == null)
+            return null; // no proper gene accession is available
+
+        String symbol = entry.getKey();
+
+        GeneIdentifier id = GeneIdentifier.of(geneId, symbol, altGeneIds.get("HGNC_ID"));
+
+        GenomeInterval txInterval = first.getTXRegion();
+        String contigName = txInterval.getRefDict().getContigIDToName().get(txInterval.getChr());
+        Contig contig = assembly.contigByName(contigName);
+        if (contig.isUnknown())
+            return null; // unknown contig
+
+        Strand strand = parseStrand(txInterval.getStrand());
+        GenomicRegion location = parseLocation(contig, strand, transcripts);
+        Collection<? extends Transcript> txs = parseTranscripts(contig, strand, transcripts);
+
+        return Gene.of(id, location, txs);
+    }
+
+    private static Collection<? extends Transcript> parseTranscripts(Contig contig, Strand strand, Collection<TranscriptModel> transcripts) {
+        Set<Transcript> txs = new HashSet<>(transcripts.size());
+
+        for (TranscriptModel tx : transcripts) {
+            // Symbol is not transferred, nor is ccdsId
+            TranscriptIdentifier txId = TranscriptIdentifier.of(tx.getAccession(), "", null);
+            if (tx.isCoding()) {
+                txs.add(parseCodingTranscript(contig, strand, txId, tx));
+            } else {
+                txs.add(parseNoncodingTranscript(contig, strand, txId, tx));
+            }
+        }
+
+        return txs;
+    }
+
+    private static Transcript parseCodingTranscript(Contig contig, Strand strand, TranscriptIdentifier txId, TranscriptModel tx) {
+        GenomeInterval txRegion = tx.getTXRegion();
+        GenomicRegion location = GenomicRegion.of(contig, strand, COORDINATE_SYSTEM, txRegion.getBeginPos(), txRegion.getEndPos());
+        List<Coordinates> exons = remapExons(tx.getExonRegions());
+        GenomeInterval cds = tx.getCDSRegion();
+        Coordinates startCodon = Coordinates.of(COORDINATE_SYSTEM, cds.getBeginPos(), cds.getBeginPos() + 3);
+        Coordinates stopCodon = Coordinates.of(COORDINATE_SYSTEM, cds.getEndPos() - 3, cds.getEndPos());
+
+        return Transcript.coding(txId, location, exons, startCodon, stopCodon);
+    }
+
+    private static Transcript parseNoncodingTranscript(Contig contig, Strand strand, TranscriptIdentifier txId, TranscriptModel tx) {
+
+        GenomeInterval txRegion = tx.getTXRegion();
+        GenomicRegion location = GenomicRegion.of(contig, strand, COORDINATE_SYSTEM, txRegion.getBeginPos(), txRegion.getEndPos());
+        List<Coordinates> exons = remapExons(tx.getExonRegions());
+
+        return Transcript.noncoding(txId, location, exons);
+    }
+
+    private static List<Coordinates> remapExons(List<GenomeInterval> exonRegions) {
+        List<Coordinates> exons = new ArrayList<>(exonRegions.size());
+        for (GenomeInterval exon : exonRegions) {
+            exons.add(Coordinates.of(COORDINATE_SYSTEM, exon.getBeginPos(), exon.getEndPos()));
+        }
+        return exons;
+    }
+
+    private Gene parseGene() {
+        Gene gene = null;
+        while (iterator.hasNext() && gene == null) {
+            // Conversion of Jannovar gene can fail and parseNext can return null. Let's make sure this method only
+            // returns null when there are no more proper genes left in the cache.
+            gene = parseNextGene(assembly, iterator.next());
+        }
+        return gene;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return next != null;
+    }
+
+    @Override
+    public Gene next() {
+        Gene current = next;
+        next = parseGene();
+        return current;
+    }
+
+}
