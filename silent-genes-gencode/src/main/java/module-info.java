dad7100d0b41bcfafa0aa34fd0b7f123ca0e7e2f
Daniel Danis <daniel.danis@jax.org>
2021-11-16 15:47:42 -0500

    Add CLI

--- silent-genes-gencode/src/main/java/module-info.java
+++ silent-genes-gencode/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module xyz.ielis.silent.genes.gencode {
     requires transitive xyz.ielis.silent.genes.model;
-    requires org.apache.commons.compress;
+    requires org.apache.commons.compress; // despite being an automatic module, this is the latest version as of Nov 2021
     requires org.slf4j;
 
     exports xyz.ielis.silent.genes.gencode.model;
